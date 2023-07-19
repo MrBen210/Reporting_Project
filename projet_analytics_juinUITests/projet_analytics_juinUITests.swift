503b5a56ca53e24711e8c81913af02075ba1f24c
ROMER Pierre-Elie <pierre.romer@adn.fr>
2023-07-19 02:21:25 +0200

    fastlane

--- projet_analytics_juinUITests/projet_analytics_juinUITests.swift
+++ projet_analytics_juinUITests/projet_analytics_juinUITests.swift
@@ -7,25 +7,25 @@
 
 import XCTest
 
-final class projet_analytics_juinUITests: XCTestCase {
-
+final class projet_analytics: XCTestCase {
+    
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
-        XCUIDevice.shared.orientation = .landscapeRight
+        XCUIDevice.shared.orientation = .landscapeLeft
         setupSnapshot(app)
         app.launch()
         snapshot("01ContentView")
@@ -39,13 +39,18 @@ final class projet_analytics_juinUITests: XCTestCase {
         app.buttons["Click"].tap()
         snapshot("03ConnectedView")
     }
-
+    
+    
+    
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
+                
             }
         }
     }
 }
+
